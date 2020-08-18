6ce123f495e82833cb1ea189ceb13394bbf64c53
Ala BEN SALAH <40735270+alabens@users.noreply.github.com>
2020-08-18 23:15:34 +0100

    Update securitygroup.tf

--- securitygroup.tf
+++ securitygroup.tf
@@ -1,43 +1,42 @@
 resource "aws_security_group" "MySecurityGroup" {
-  vpc_id      = aws_vpc.MyVpc.id
-  name        = "MySecurityGroup"
-  description = "security group that allows ssh and all egress traffic"
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+                                                  vpc_id      = aws_vpc.MyVpc.id
+                                                  name        = "MySecurityGroup"
+                                                  description = "security group that allows ssh and all egress traffic"
+                                                  egress {
+                                                          from_port   = 0
+                                                          to_port     = 0
+                                                          protocol    = "-1"
+                                                          cidr_blocks = ["0.0.0.0/0"]
+                                                         }
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+                                                  ingress {
+                                                           from_port   = 22
+                                                           to_port     = 22
+                                                           protocol    = "tcp"
+                                                           cidr_blocks = ["0.0.0.0/0"]
+                                                          }
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+                                                  ingress {
+                                                           from_port   = 80
+                                                           to_port     = 80
+                                                           protocol    = "tcp"
+                                                           cidr_blocks = ["0.0.0.0/0"]
+                                                          }
 
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 9000
-    to_port     = 9000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  tags = {
-    Name = "MySecurityGroup"
-  }
-}
+                                                  ingress {
+                                                           from_port   = 8080
+                                                           to_port     = 8080
+                                                           protocol    = "tcp"
+                                                           cidr_blocks = ["0.0.0.0/0"]
+                                                          }
+  
+                                                  ingress {
+                                                           from_port   = 9000
+                                                           to_port     = 9000
+                                                           protocol    = "tcp"
+                                                           cidr_blocks = ["0.0.0.0/0"]
+                                                          }
+  
+                                                  tags = {Name = "MySecurityGroup"}
+                                              }
 
